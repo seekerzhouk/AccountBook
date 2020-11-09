bd55a54b801a05c4d0b625d548b2ffb05d6f16db
seekerzhouk <seekerzhouk@gmail.com>
2020-11-09 23:24:26 +0800

    [新增] PieView.kt增加点击事件。

--- app/src/main/java/com/seekerzhouk/accountbook/ui/customize/PieView.kt
+++ app/src/main/java/com/seekerzhouk/accountbook/ui/customize/PieView.kt
@@ -7,16 +7,13 @@ import android.text.Layout
 import android.text.StaticLayout
 import android.text.TextPaint
 import android.util.AttributeSet
-import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.RequiresApi
 import com.seekerzhouk.accountbook.R
 import com.seekerzhouk.accountbook.room.home.Sector
-import com.seekerzhouk.accountbook.utils.MyLog
 import java.text.NumberFormat
-import kotlin.math.cos
-import kotlin.math.min
-import kotlin.math.sin
+import kotlin.math.*
 
 class PieView : View {
     private val tag = PieView::class.java.name
@@ -55,7 +52,7 @@ class PieView : View {
         context.getColor(R.color.pie_view_8)
     )
 
-    // 初始绘制角度
+    // 初始绘制角度,弧度制
     private var startAngle = 0F
         set(value) {
             field = value
@@ -69,7 +66,6 @@ class PieView : View {
             field = value
             initData(field)
             invalidate()
-            MyLog.i(tag, "set sectorList : $value")
         }
 
     // 总宽高
@@ -90,46 +86,104 @@ class PieView : View {
     // 圆心在半径方向上的偏移量
     private val offR: Float = 6F
 
+    private var pieR: Float = 0F
+
     // 辅助线的长度
     private val aLength: Float = 80F
 
     // 百分数工具
     private val numberFormat: NumberFormat = NumberFormat.getPercentInstance()
 
+    private var clickPosition = -1
+
+    private var listener: OnSectorClickListener? = null
+
+    fun setOnSectorClickListener(l: OnSectorClickListener) {
+        this.listener = l
+    }
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         totalWidth = w
         totalHeight = h
+        pieR = (min(totalWidth, totalHeight) / 2 * 0.5).toFloat()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         var width = measuredWidth
-//        var height = (measuredWidth * 0.9).toInt()
         var height = measuredWidth
         width = resolveSize(width, widthMeasureSpec)
         height = resolveSize(height, heightMeasureSpec)
         setMeasuredDimension(width, height)
     }
 
+    private var isActionDown = false
+    private var clickX = 0F
+    private var clickY = 0F
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                isActionDown = true
+                clickX = event.x - (totalWidth / 2).toFloat()
+                clickY = event.y - totalHeight * 0.55.toFloat()
+
+                if (hypot(clickX, clickY) > pieR + offR) {
+                    return false
+                }
+                invalidate()
+                return true
+            }
+
+            MotionEvent.ACTION_CANCEL -> {
+                isActionDown = false
+                clickX = 0F
+                clickY = 0F
+                invalidate()
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                isActionDown = false
+                clickX = 0F
+                clickY = 0F
+                invalidate()
+                performClick()
+                return true
+            }
+            else -> return super.onTouchEvent(event)
+        }
+
+    }
+
+    override fun performClick(): Boolean {
+        listener?.apply {
+            if (clickPosition == -1) {
+                return super.performClick()
+            } else {
+                onCLick(clickPosition)
+                clickPosition = -1
+            }
+        }
+        return super.performClick()
+    }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         var currentStartAngle: Float = startAngle
-        val r: Float = (min(totalWidth, totalHeight) / 2 * 0.5).toFloat()
-        rectF.set(-r, -r, r, r)
+        rectF.set(-pieR, -pieR, pieR, pieR)
         canvas?.translate((totalWidth / 2).toFloat(), totalHeight * 0.55.toFloat())
-        for (pie in sectorList) {
-            if (pie.moneySum == 0F) {
+        for (i in sectorList.indices) {
+            if (sectorList[i].moneySum == 0F) {
                 continue
             }
             // 第一段引线的角度
-            val lineAngle = currentStartAngle + pie.angle / 2
+            val lineAngle = currentStartAngle + sectorList[i].angle / 2
             // 第一段引线从圆弧开始，下面是第一段引线和圆弧的交点
-            val x1 = (r + offR) * cos(Math.toRadians(lineAngle.toDouble())).toFloat()
-            val y1 = (r + offR) * sin(Math.toRadians(lineAngle.toDouble())).toFloat()
+            val x1 = (pieR + offR) * cos(lineAngle)
+            val y1 = (pieR + offR) * sin(lineAngle)
             // 一二段引线的交点，第二段引线的起点是第一段引线的终点
-            val x2 = (r + aLength) * cos(Math.toRadians(lineAngle.toDouble())).toFloat()
-            val y2 = (r + aLength) * sin(Math.toRadians(lineAngle.toDouble())).toFloat()
+            val x2 = (pieR + aLength) * cos(lineAngle)
+            val y2 = (pieR + aLength) * sin(lineAngle)
             // 第二段引线终点
             val x3: Float = if (x1 > 0) (x2 + aLength) else (x2 - aLength)
             val y3: Float = y2
@@ -138,12 +192,31 @@ class PieView : View {
             val textY: Float = if (y3 > 0) y3 else (y3 - 40)
             // 画扇形
             canvas?.save()
-            canvas?.translate(
-                offR * cos(Math.toRadians(lineAngle.toDouble())).toFloat(),
-                offR * sin(Math.toRadians(lineAngle.toDouble())).toFloat()
-            )
-            mPaint.color = pie.color.toInt()
-            canvas?.drawArc(rectF, currentStartAngle, pie.angle, true, mPaint) // 画饼状图
+            var clickAngle = atan2(clickY, clickX)
+            if (clickAngle < 0) {
+                clickAngle = (clickAngle + Math.PI * 2).toFloat()
+            }
+            if (isActionDown
+                && clickAngle > currentStartAngle
+                && clickAngle < (currentStartAngle + sectorList[i].angle)
+                && hypot(clickX, clickY) <= pieR + offR
+            ) {
+                clickPosition = i
+            } else {
+                canvas?.translate(
+                    offR * cos(lineAngle),
+                    offR * sin(lineAngle)
+                )
+            }
+
+            mPaint.color = sectorList[i].color
+            canvas?.drawArc(
+                rectF,
+                Math.toDegrees(currentStartAngle.toDouble()).toFloat(),
+                Math.toDegrees(sectorList[i].angle.toDouble()).toFloat(),
+                true,
+                mPaint
+            ) // 画饼状图
             canvas?.restore()
 
             // 画线和文字
@@ -153,7 +226,8 @@ class PieView : View {
             path.lineTo(x2, y2)
             path.lineTo(x3, y3)
             canvas?.drawPath(path, linePaint)
-            val string = pie.consumptionType + "(" + numberFormat.format(pie.percentage) + ")"
+            val string =
+                sectorList[i].consumptionType + "(" + numberFormat.format(sectorList[i].percentage) + ")"
             val staticLayout: StaticLayout = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
                 StaticLayout(
                     string, textPaint, 600, Layout.Alignment.ALIGN_NORMAL,
@@ -171,7 +245,7 @@ class PieView : View {
             canvas?.restore()
 
             // 当前所处角度
-            currentStartAngle += pie.angle
+            currentStartAngle += sectorList[i].angle
         }
     }
 
@@ -193,8 +267,12 @@ class PieView : View {
         for (pie in mData) {
             val percentage = pie.moneySum / sumValue
             pie.percentage = percentage.toFloat()
-            val angle = percentage * 360
+            val angle = percentage * 2 * Math.PI
             pie.angle = angle.toFloat()
         }
+    }
+
+    interface OnSectorClickListener {
+        fun onCLick(i: Int)
     }
 }
